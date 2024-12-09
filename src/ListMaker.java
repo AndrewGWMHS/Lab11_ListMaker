a281961c9511bdf686f1db98af3320325e970ed4
AndrewGWMHS <goff.614615@swl.k12.oh.us>
2024-12-09 08:55:43 -0500

    2

--- src/ListMaker.java
+++ src/ListMaker.java
@@ -6,18 +6,29 @@ public class ListMaker {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         ArrayList <String> fruit = new ArrayList<String>();
+        boolean playAgain = true;
 
         fruit.addAll(Arrays.asList("Apple", "Banana", "Pear", "Strawberry", "Blueberry", "Grape", "Peach", "Pineapple", "Orange", "Melon", "Mango", "Kiwi", "Coconut", "Cherry", "Apricot", "Lemon"));
 
-        System.out.println(fruit);
+        do {
+            printList(fruit);
+            String menu = InputHelper.getRegExString(scan, "Options:  A  -  Add an item to the list    D  - Delete an item from the list     P  -  Print the list    Q  -   Quit the program", "[AaDdPpQq]");
+            switch (menu) {
+                case "A":
+                    addList(scan, fruit);
+                    break;
+                case "D":
+                    deleteList(scan, fruit);
+                    break;
+                case "P":
+                    printList(fruit);
+                    break;
+                case "Q":
+                    quitList(scan);
+                    break;
+            }
 
-        addList(scan, fruit);
-
-        System.out.println(fruit);
-
-        deleteList(scan, fruit);
-
-        System.out.println(fruit);
+        } while (playAgain = false);
     }
 
 
@@ -29,16 +40,20 @@ public class ListMaker {
 
     private static void deleteList(Scanner scan, ArrayList fruit) {
         int indexRemove;
-        indexRemove = InputHelper.getRangedInt(scan, "What item do you want to remove? (Enter index)", 0, fruit.size());
+        indexRemove = InputHelper.getRangedInt(scan, "What item do you want to remove? (Enter index)", 0, fruit.size() - 1);
         fruit.remove(indexRemove);
     }
 
     private static void printList(ArrayList fruit) {
         for(int i = 0; i < fruit.size(); i++) {
+            System.out.println(i + " - "  + fruit.get(i));
         }
     }
 
-    private static boolean quitList(Scanner scan) {
-        return false;
+    private static void quitList(Scanner scan) {
+        boolean quit = InputHelper.getYNConfirm(scan, "Are you sure you want to quit? [Y/N]");
+        if (quit) {
+            System.exit(0);
+        }
     }
 }
